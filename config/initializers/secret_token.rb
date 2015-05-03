dabda2e312718adb5354bfd9acfacca0f0f3ee54
donsalvadori <csalvi42@gmail.com>
2015-05-03 17:13:02 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Muse::Application.config.secret_key_base = '027576467a10bf9a1e1e440b7bcc0db3a9d14ca3148bd9b332d5c098c6b89766f2ec8385536e64523ad8dbe686c43a2d07aae5d19cb21d51de9d7050ea50f5a6'
